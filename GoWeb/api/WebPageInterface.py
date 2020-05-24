6c75948daac53e5b3343482dedd14186c474743b
xpEXIA <41253379+xpEXIA@users.noreply.github.com>
2020-05-24 22:33:44 +0800

    ajax request test homework

--- GoWeb/api/WebPageInterface.py
+++ GoWeb/api/WebPageInterface.py
@@ -3,14 +3,15 @@
 
 
 
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect, HttpResponse
 from datetime import datetime
 
 def pageA(request):
 
     return render(request,
                   'pageA.html',
-                  {'name':datetime.now(),
+                  {'time':datetime.now(),
                    'dota':[
                        {'id':'1','position':'carry','role':'luna'},
                        {'id':'2','position':'mid','role':'strom'},
@@ -18,3 +19,17 @@ def pageA(request):
                    ]
                    }
                   )
+
+
+def pageB(request):
+
+    return render(request, 'pageB.html')
+
+
+def aJaxTest(request):
+
+    print(request.POST.get('title'))
+    if request.POST.get('title') == 'luna':
+        return HttpResponse('0')
+    else:
+        return HttpResponse('你个智障')
