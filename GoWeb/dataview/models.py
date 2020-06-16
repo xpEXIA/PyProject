2750221b74f4212a5365a82e787b4b4b18139838
xpEXIA <41253379+xpEXIA@users.noreply.github.com>
2020-06-16 17:46:52 +0800

    add....

---
+++ GoWeb/dataview/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class UserGroup(models.Model):
+
+    group = models.CharField(max_length=32)
+
+class UserInfo(models.Model):
+
+    nid = models.AutoField(primary_key=True)
+    user = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    age = models.IntegerField(default=0)
---
+++ GoWeb/dataview/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, redirect, HttpResponse
+from datetime import datetime
+from GoWeb.settings import logger
+
+# Create your views here.
